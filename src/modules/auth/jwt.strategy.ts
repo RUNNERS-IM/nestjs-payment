92fac102a73d94a397846ed990f3171b54e8342a
Sunwook KIm <sunus7@naver.com>
2022-06-16 23:11:37 +0900

    no message

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.authConfig.publicKey,
+      secretOrKey: configService.authConfig.secretKey,
     });
   }
 
